52eda200950895a553499ef1914aed859e89054d
Kenji Mukai <kensuka21@gmail.com>
2018-10-04 13:18:24 -0400

    first commit

---
+++ Repository/EmployeeRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class EmployeeRepository
+    {
+        public MvcCrudDBEntities db = new MvcCrudDBEntities();
+
+        public void Create(Employee employee)
+        {
+            db.Employees.Add(employee);
+            db.SaveChanges();
+        }
+
+        public Employee GetByEmployeeId(int id)
+        {
+            var employee = db.Employees.FirstOrDefault(x => x.EmployeeID == id);
+            return employee;
+        }
+
+        public IQueryable<Employee> GetAllEmployees()
+        {
+            return db.Employees;
+        }
+
+        public void Delete(int id)
+        {
+            var employee = GetByEmployeeId(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+        }
+
+        public void Update(Employee employee)
+        {
+            db.Employees.Attach(employee);
+            db.Entry(employee).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
---
+++ WebApplication2/Controllers/EmployeesController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Repository;
+
+namespace WebApplication2.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        private EmployeeRepository _repository = new EmployeeRepository();
+
+        // GET: api/Employees
+        public IQueryable<Employee> GetEmployees()
+        {
+            return _repository.GetAllEmployees();
+        }
+
+        // GET: api/Employees/5
+        [ResponseType(typeof(Employee))]
+        public IHttpActionResult GetEmployee(int id)
+        {
+            Employee employee = _repository.GetByEmployeeId(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+        // PUT: api/Employees/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutEmployee(int id, Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != employee.EmployeeID)
+            {
+                return BadRequest();
+            }
+
+            _repository.Update(employee);
+
+            return Ok(employee);
+        }
+
+        // POST: api/Employees
+        [ResponseType(typeof(Employee))]
+        public IHttpActionResult PostEmployee(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _repository.Create(employee);
+
+            return Ok(employee);
+        }
+
+        // DELETE: api/Employees/5
+        [ResponseType(typeof(Employee))]
+        public IHttpActionResult DeleteEmployee(int id)
+        {
+            var employee = _repository.GetByEmployeeId(id);
+            if(employee == null)
+            {
+                return NotFound();
+            }
+
+            _repository.Delete(id);
+
+            return Ok();
+        }
+    }
+}
